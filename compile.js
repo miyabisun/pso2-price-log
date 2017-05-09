182d61a608705fad16773a059d27079cd56d5509
miyabisun <miyabi.ooh@gmail.com>
2017-05-09 09:44:52 +0900

    add dist directory.

--- compile.js
+++ compile.js
@@ -1,14 +1,7 @@
 var flags = [];
 var enclose = require("enclose").exec;
 
-try {
-  require.resolve("npm");
-} catch (error) {
-  console.log("Failed to require('npm')");
-  console.log("Please run 'npm install' here");
-  process.exit(1);
-}
-
 flags.push("--config", "./config.js");
+flags.push("--output", "./tmp/pso2-price-log.exe");
 flags.push("./windows.js");
 enclose(flags);
--- functions/do-update.ls
+++ functions/do-update.ls
@@ -10,6 +10,6 @@ module.exports = ({particle, status}, cb)->
   else
     price = particle.message |> parse-num
     update-row status.sheet, row.\名称, price, (->)
-    console.info "update: #{row.\名称} => #{price.to-locale-string!}"
+    console.info "update: #{row.\名称} => #{price.to-locale-string \ja-JP, maximum-fraction-digits: 3}"
     cb!
 
